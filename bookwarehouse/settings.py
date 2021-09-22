e1478aae4e007796bc0dbf37b9a5c9a319a1d774
shubhamthakar <62199528+shubhamthakar@users.noreply.github.com>
2021-09-22 14:24:03 +0530

    Configured postgis db

--- bookwarehouse/settings.py
+++ bookwarehouse/settings.py
@@ -11,10 +11,19 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+if os.name == 'nt':
+    import platform
+    OSGEO4W = r"C:\OSGeo4W"
+    if '64' in platform.architecture()[0]:
+        OSGEO4W += "64"
+    assert os.path.isdir(OSGEO4W), "Directory does not exist: " + OSGEO4W
+    os.environ['OSGEO4W_ROOT'] = OSGEO4W
+    os.environ['GDAL_DATA'] = OSGEO4W + r"\share\gdal"
+    os.environ['PROJ_LIB'] = OSGEO4W + r"\share\proj"
+    os.environ['PATH'] = OSGEO4W + r"\bin;" + os.environ['PATH']
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -37,6 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
+    'geoapp'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +86,13 @@ WSGI_APPLICATION = 'bookwarehouse.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'spatialdb',
+        'USER': 'postgres',
+        'HOST': 'localhost',
+        'PASSWORD': 'postgres',
+        'PORT': '5432'
+
     }
 }
 
--- geoapp/models.py
+++ geoapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.gis.db import models
 
+class Bank(models.Model):
+    name = models.CharField(max_length=20)
+    address = models.CharField(max_length=128)
+    zip_code = models.CharField(max_length=5)
+    poly = models.PolygonField()
+
+    def __str__(self):
+        return self.name
 # Create your models here.
